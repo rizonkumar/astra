ab8c62333c6cacf680fc8610b0322366ab7174f3
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2026-01-23 10:00:15 +0530

    Add ProjectIdView Component for Project Details Display
    - Created a new ProjectIdView component to enhance the display of project details, featuring a tabbed interface for 'Code' and 'Preview' views.
    - Updated ProjectIdPage to utilize the new ProjectIdView component, improving the structure and user experience of project-specific pages.
    - Implemented state management for active tabs within the ProjectIdView component, allowing users to switch between different views seamlessly.

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,10 +1,13 @@
+import { ProjectIdView } from "@/features/projects/components/project-id-view";
+import { Id } from "../../../../convex/_generated/dataModel";
+
 const ProjectIdPage = async ({
   params,
 }: {
   params: Promise<{ projectId: string }>;
 }) => {
   const { projectId } = await params;
-  return <div>ProjectIdPage: {projectId}</div>;
+  return <ProjectIdView projectId={projectId as Id<"projects">} />;
 };
 
 export default ProjectIdPage;
